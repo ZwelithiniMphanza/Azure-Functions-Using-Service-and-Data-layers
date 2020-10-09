3a05df6659dce82e94daf054eedd3102f6f4a218
Zwelithini Mphanza <z.mphanza@gmail.com>
2020-10-09 16:35:03 +0200

    added person model

--- TestApp.Data/Models/PersonModel.cs
+++ TestApp.Data/Models/PersonModel.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Cosmos.Table;
 using System;
+using System.Collections.Generic;
 using TestApp.Data.Helpers;
+using Newtonsoft.Json;
 
 namespace TestApp.Data.Models
 {
@@ -10,6 +12,30 @@ namespace TestApp.Data.Models
 		{
 			PartitionKey = Constants.TableDBConstant.TEST_APP_PARTITION;
 			RowKey = Guid.NewGuid().ToString();
+		}
+
+		[JsonProperty("name")]
+		public string Name;
+
+		[JsonProperty("age")]
+		public int Age;
+
+		[JsonProperty("height")]
+		public double Height;
+
+		[JsonProperty("married")]
+		public bool Married;
+
+		[JsonProperty("skills")]
+		public List<Skills> PersonSkills;
+
+		public class Skills
+		{
+			[JsonProperty("id")]
+			public int Id;
+
+			[JsonProperty("skill")]
+			public string Skill;
 		}
 	}
 }
